0a2a6c2a26b9e925bf92ce2f3e243a209e49aa74
Zoe8888 <zoeerispe@gmail.com>
2021-04-13 09:33:06 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello Everybody")
+_pi = 3.14  # value of pi
+r = float(input("insert radius value in cm: "))
+a = _pi * (r ** 2)
+print("Area of circle is", a, "cm^")
+d = r * 2
+print("Diameter of circle is", d, "cm")
+c = (2 * _pi) * r
+print("Circumference of circle is", c, "cm")
